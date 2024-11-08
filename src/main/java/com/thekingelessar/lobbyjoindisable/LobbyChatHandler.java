ec32359d5b3f1f276f7a0d0880fe3df1e4d54ebb
TheNathanSpace <nathan@thenathan.space>
2024-11-08 16:24:58 -0700

    idk

--- src/main/java/com/thekingelessar/lobbyjoindisable/LobbyChatHandler.java
+++ src/main/java/com/thekingelessar/lobbyjoindisable/LobbyChatHandler.java
@@ -6,15 +6,18 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class LobbyChatHandler
 {
-    
     @SubscribeEvent (priority = EventPriority.NORMAL, receiveCanceled = true)
     public void onEvent(ClientChatReceivedEvent chatEvent)
     {
         String fullMessage = chatEvent.message.getUnformattedText();
-        if (fullMessage.contains("joined the lobby!") || fullMessage.contains("sled into the lobby!"))
+        if (fullMessage.contains("joined the lobby!") || fullMessage.contains("sled into the lobby!") || fullMessage.contains("slid into the lobby!") || fullMessage.contains("/animations"))
+        {
+            chatEvent.setCanceled(true);
+        }
+        
+        if (fullMessage.contains("You tipped") || fullMessage.contains("You were tipped"))
         {
             chatEvent.setCanceled(true);
         }
     }
-    
 }
